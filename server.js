f2b46e4d31ca0bf1accfb4e932f9b22e778672a3
Josiah-Canada <josiahcanada@yahoo.com>
2022-06-22 14:34:37 -0400

    log

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -7,10 +8,27 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'Newsqlwhodis!97',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
+
 // Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
 });
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
